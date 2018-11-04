84f4d16a6953883166befe4c91e0d6f72bcc7cef
rohit <rohit.3mindsdigital@gmail.com>
2018-11-04 11:06:55 +0530

    mongoose & dotenv installed and configured

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
+
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -16,5 +19,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+/*************************************
+    Mongoose Connect
+*************************************/
+mongoose.connect(process.env.MONGOSERVER)
+    .then(con => {
+        console.log("Successfully connected to MongoDB");
+    })
+    .catch(error => {
+        console.error(`Error connecting to MongoDB ${error}`);
+        process.exit(0);
+    });
 
 module.exports = app;
